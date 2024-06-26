3f6a6d999870dddb9e0091103a9901e17bd13451
MrTrazan <147327557+MrTrazan@users.noreply.github.com>
2024-06-26 21:43:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  TOKEN: "",
+  ownerID: ["871250585498845184", ""],
+  botInvite: "",
+  supportServer: "",
+  mongodbURL: "mongodb+srv://shiva:shiva@cluster0.do0cdb4.mongodb.net/?retryWrites=true&w=majority",
+  status: 'CodeX GG',
+  commandsDir: './commands',
+  language: "en",
+  embedColor: "00fbff",
+  errorLog: "",
+
+
+  sponsor: {
+    status: true,
+    url: "https://dsc.gg/codxdev",
+  },
+
+  voteManager: {
+    status: false,
+    api_key: "",
+    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
+    vote_url: "",
+  },
+
+  shardManager: {
+    shardStatus: false
+  },
+
+  playlistSettings: {
+    maxPlaylist: 10,
+    maxMusic: 75,
+  },
+
+  opt: {
+    DJ: {
+      commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle']
+    },
+
+    voiceConfig: {
+      leaveOnFinish: false,
+      leaveOnStop: false,
+      leaveOnEmpty: {
+        status: true,
+        cooldown: 10000000,
+      },
+
+    },
+
+    maxVol: 150,
+
+  }
+}
